e31f65fe45add2afa8e04abe24cee4588c2e9eff
Alexandre Yamassaka <alexandrecmska@gmail.com>
2024-11-07 13:04:03 -0300

    added main.cpp in order to test lagrange method.

--- interpolation/main.cpp
+++ interpolation/main.cpp
@@ -1,14 +1,16 @@
 #include "interpolation.hpp"
 
+
 int main(){ 
 
-    vector<pair<double, double> points = {
+    vector<pair<double, double>> points = {
         {1.0, 0.8415},
         {1.3, 1.2526},
         {1.7, 1.6858},
         {2.0, 1.8186}
     };
     Interpolator interpolacao(points, "lagrange");
+    interpolacao.interpolate(1.5);
 
     return 0;
 }
