77646f793bb8e6a7bc812bd1ebb869696b266681
Alexandre Yamassaka <alexandrecmska@gmail.com>
2024-11-07 12:58:23 -0300

    new methods added

--- interpolation/interpolation.cpp
+++ interpolation/interpolation.cpp
@@ -1,26 +1,34 @@
 #include "interpolation.hpp"
 
-Interpolador(const vector<pair<double, double>>& points, const string& method){
+Interpolator::Interpolator(const vector<pair<double, double>>& points, const string& method):points(points), method(method){
+    if(method == "newton"){
+        calculateNewtonCoefficients();
+    }
+}
 
+double Interpolator::interpolate(double x){
+    if(method == "lagrange"){
+        return lagrangeInterpolate(x);
+    }
+    else if(method == "newton"){
+        return newtonInterpolate(x);
+    }
+    else{
+        throw invalid_argument("Unknow interpolation method");
+    }
 }
 
 double Interpolator::lagrangeInterpolate(double x){
-    
+    double interpolationPoint = 0;
+    for(int i = 0; i<points.size();i++){
+        double product = 1;
+        for(int j = 0; j<points.size();j++){
+            if(j!=i){
+                product *= (x-points[j].first)/(points[i].first-points[j].first);
+            }
+        }
+        interpolationPoint += points[i].second * product;
+    }
+    return interpolationPoint;
 }
 
-
-double interpolacaoLagrange(int numeroPontos, const vector<double>& x,const vector<double>& y, double pontoInterpolar){
-	double pontoInterpolado = 0;
-	for(int i = 0; i<numeroPontos;i++){
-		double c = 1; double d = 1;
-		for(int j = 0; j<numeroPontos; j++){
-			if(j!=i){
-				c *= (pontoInterpolar-x[j]);
-				d *= (x[i]-x[j]);
-			}
-		}
-		pontoInterpolado += y[i]*(c/d);
-
-	}
-	return pontoInterpolado;
-}
